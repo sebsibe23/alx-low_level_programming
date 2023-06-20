25ded3e75e6607ceda6b511ce055c8a69881ecf8
sebsibe23 <sebsibe1990@proton.me>
2023-06-20 11:59:35 -0700

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 
 /**
  * _islower - check to see if the character is lowercase
- * @c: the char needs to be verified
- * If char is lowercase, return 1; otherwise, return 0.
+ * @q: the char needs to be verified
+ * If q is lowercase, return 1; otherwise, return 0.
  */
 
 int _islower(int q)
