a6a0204ddb6eca70fe093fa3b6566cc889a34196
sebsibe23 <sebsibe1990@proton.me>
2023-06-20 12:31:10 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for the presence of an alphabetic character
+ * _isalpha- checks for the presence of an alphabetic character
  * @d: the character to be examined
  * Return: 1 if d is a letter, 0 otherwise
  */
