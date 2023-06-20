f5bd5d0cf12be024dd9b5d95b7755eacb1a258a0
sebsibe23 <sebsibe1990@proton.me>
2023-06-20 13:49:02 -0700

    final

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
- *        separated by a comma followed by a space.
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2.
+ *        separated by  comma and followed by  space.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
+	int m;
+	unsigned long fb1 = 0, fb2 = 1, tot;
 
-	for (count = 0; count < 50; count++)
+	for (m = 0; m < 50; m++)
 	{
-		sum = fib1 + fib2;
-		printf("%lu", sum);
+		tot = fb1 + fb2;
+		printf("%lu", tot);
 
-		fib1 = fib2;
-		fib2 = sum;
+		fb1 = fb2;
+		fb2 = tot;
 
-		if (count == 49)
+		if (m == 49)
 			printf("\n");
 		else
 			printf(", ");
--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of the even-valued terms
- * followed by a new line
- * Return: Always 0 (Success)
+ * main - finds and prints  sum of the even-valued terms
+ * followed by a new line.
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
 	int i;
-	unsigned long int j, k, next, sum;
+	unsigned long int num1, num2, next, tot1;
 
-	j = 1;
-	k = 2;
-	sum = 0;
+	num1 = 1;
+	num2 = 2;
+	tot1 = 0;
 
 	for (i = 1; i <= 33; ++i)
 	{
-		if (j < 4000000 && (j % 2) == 0)
+		if (num1 < 4000000 && (num1 % 2) == 0)
 		{
-			sum = sum + j;
+			tot1 = tot1 + num1;
 		}
-		next = j + k;
-		j = k;
-		k = next;
+		next = num1 + num2;
+		num1 = num2;
+		num2 = next;
 	}
 
-	printf("%lu\n", sum);
+	printf("%lu\n", tot1);
 
 	return (0);
 }
