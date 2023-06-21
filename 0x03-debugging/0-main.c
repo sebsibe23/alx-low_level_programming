12006e7afb5622ae340416076539dd54c93e13f6
sebsibe23 <sebsibe1990@proton.me>
2023-06-21 16:11:49 -0700

    0x03. C - Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main - Test function for negative or positive. 
+* Return: 0 for success.
+*/
+int main(void)
+{
+int h;
+h = 0;
+positive_or_negative(h);
+return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positive_or_negative(int w);
+int largest_number(int q, int w, int r);
+void print_remaining_days(int mm_, int dd_, int yy_);
+int convert_day(int mm_, int dd_);
+#endif
