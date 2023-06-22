db9ef974d38276c4497403cb9ae00513c3297746
sebsibe23 <sebsibe1990@proton.me>
2023-06-21 23:01:53 -0700

    main fixed

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
+void positive_or_negative(int j);
+int largest_number(int q, int w, int e);
+void print_remaining_days(int mm_, int dd_, int yy_);
+int convert_day(int month, int dd_);
 
 #endif /* MAIN_H */
