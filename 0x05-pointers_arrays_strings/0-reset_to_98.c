d4cd34719b4e3689ec1931389bd03ad36661bb21
sebsibe23 <sebsibe1990@proton.me>
2023-06-26 10:12:17 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that accepts an int pointer as a parameter
+ * and updates the value it points to to 98
+ * @d: input
+ * Return: d
+ */
+void reset_to_98(int *d)
+{
+	*d = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int *d);
+void swap_int(int *d, int *f);
+int _strlen(char *t);
+void _puts(char *chr);
+void print_rev(char *x);
+void rev_string(char *x);
+void puts2(char *chr);
+void puts_half(char *chr);
+void print_array(int *c, int h);
+char *_strcpy(char *dest, char *chr);
+
+#endif
