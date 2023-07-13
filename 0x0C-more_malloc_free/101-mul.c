932d9cec7c2c946106c98129c8d5c335f9b58c56
sebsibe23 <sebsibe1990@proton.me>
2023-07-13 09:55:47 -0700

    last q

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,37 +1,37 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#define ERR_MSG "Error"
+#include "main.h"
+
 /**
- * is_digit - checks if the string contains a non-digit char
- * @strs: string to be evaluated
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
-int is_digit(char *strs)
+int is_digit(char *s)
 {
-	int j = 0;
+	int i = 0;
 
-	while (strs[j])
+	while (s[i])
 	{
-		if (strs[j] < '0' || strs[j] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		j++;
+		i++;
 	}
 	return (1);
 }
 
 /**
- * _strlen - returns a length of a string
- * @strs: the string to be evaluate
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: return the length of the string
+ * Return: the length of the string
  */
-int _strlen(char *strs)
+int _strlen(char *s)
 {
 	int i = 0;
 
-	while (strs[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
@@ -48,52 +48,52 @@ void errors(void)
 }
 
 /**
- * main - multiplies the two positive numbers.
- * @argc: the number of arguments.
- * @argv: the array of arguments.
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: return always 0 (Success).
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	char *str_s1, *str_s2;
-	int str_len1, str_len2, str_len, i, str_carry, digit_1, digit_2, *strresult, stra = 0;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	str_s1 = argv[1], str_s2 = argv[2];
-	if (argc != 3 || !is_digit(str_s1) || !is_digit(str_s2))
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	str_len1 = _strlen(str_s1);
-	str_len2 = _strlen(str_s2);
-	str_len = str_len1 + str_len2 + 1;
-	strresult = malloc(sizeof(int) * str_len);
-	if (!strresult)
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
 		return (1);
-	for (i = 0; i <= str_len1 + str_len2; i++)
-		strresult[i] = 0;
-	for (str_len1 = str_len1 - 1; str_len1 >= 0; str_len1--)
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit_1 = str_s1[str_len1] - '0';
-		str_carry = 0;
-		for (str_len2 = _strlen(str_s2) - 1; str_len2 >= 0; str_len2--)
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit_2 = str_s2[str_len2] - '0';
-			str_carry += strresult[str_len1 + str_len2 + 1] + (digit_1 * digit_2);
-			strresult[str_len1 + str_len2 + 1] = str_carry % 10;
-			str_carry /= 10;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		if (str_carry > 0)
-		strresult[str_len1 + str_len2 + 1] += str_carry;
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < str_len - 1; i++)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (strresult[i])
-			stra = 1;
-		if (stra)
-			_putchar(strresult[i] + '0');
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-	if (!stra)
-		putchar('0');
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
-	free(strresult);
+	free(result);
 	return (0);
 }
