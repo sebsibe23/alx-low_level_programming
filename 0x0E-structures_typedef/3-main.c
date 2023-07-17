605c7c74a68e3844b034dc8d3203d0ed719288cf
sebsibe23 <sebsibe1990@proton.me>
2023-07-17 04:29:54 -0700

    q3

--- 0x0E-structures_typedef/3-main.c
+++ 0x0E-structures_typedef/3-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	strdog_t my_dog;
+	dog_t my_dog;
 
 	my_dog.name = "Poppy";
 	my_dog.age = 3.5;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,15 +16,14 @@ struct dog
 	char *owner;
 };
 /**
- * strdog_t - typedef for struct dog
+ * dog_t - typedef for struct dog
  */
-typedef struct dog strdog_t;
+typedef struct dog dog_t;
 
-void init_dog(struct dog *strd, char *name, float age, char *owner);
-void print_dog(struct dog *strd);
-strdog_t *new_dog(char *name, float age, char *owner);
-void free_dog(strdog_t *strd);
-char *_strcpy(char *strdest, char *strsrc);
-int _strlen(char *str);
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
