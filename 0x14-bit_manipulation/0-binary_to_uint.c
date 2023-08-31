cf7d49070809d7e012c74db5626f7250277cfaa7
sebsibe23 <sebsibe1990@proton.me>
2023-08-31 11:32:29 -0700

    last q1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,18 +13,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int intresult = 0;
+	unsigned int val_int = 0;
+	int e = 0;
 
 	if (b == NULL)
-		return 0;
-
-	while (*b)
 	{
-
-		if (*b != '0' && *b != '1')
-			return 0;
-		intresult = (intresult << 1) + (*b - '0');
-		b++;
+		return (0);
 	}
-	return intresult;
+
+	while (b[e] == '0' || b[e] == '1')
+	{
+		val_int <<= 1;
+		val_int += b[e] - '0';
+		e++;
+	}
+
+	return (val_int);
 }
