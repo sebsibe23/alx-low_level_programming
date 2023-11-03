5709a4e645cf886e51926ac2f7053c549bb12901
sebsibe23 <sebsibe1990@proton.me>
2023-11-03 01:31:40 -0700

    final submit q6 final

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,20 +1,20 @@
 #include "hash_tables.h"
 /**
- * hash_table_print - Prints the key/value pairs in the hash 
+ * hash_table_print - Prints the key/value pairs in the hash
  * table in the order they appear in the array.
  *
  * @ht: A pointer to the hash table.
  *            This is the hash table whose key/value pairs will be printed.
  *
- * This function iterates over the array of the hash table. 
+ * This function iterates over the array of the hash table.
  * For each entry in the array,
- * it traverses the linked list and prints the key/value pairs. 
+ * it traverses the linked list and prints the key/value pairs.
  * The pairs are separated by
  * commas and enclosed in curly braces. If the hash table is NULL,
  * the function simply returns
  * without doing anything.
  *
- * There is no return value. The function produces output 
+ * There is no return value. The function produces output
  * by printing to the standard output.
  */
 void hash_table_print(const hash_table_t *ht)
