2d60a40fbfc230f72409f22d0b32632aaca97d16
sebsibe23 <sebsibe1990@proton.me>
2023-11-03 01:11:48 -0700

    q5

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Retrieves a value associated with a key from the hash table.
+ * hash_table_get - Retrieves a value associated
+ * with a key from the hash table.
  * @ht: A pointer to the hash table array.
  * @key: The key, which is a non-empty string.
  *
- * Return: The value associated with the key, or NULL if the key can't be found.
+ * Return: The value associated with the key,
+ * or NULL if the key can't be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
