ae054f8bcae545d84c9cfe2a609febf612e3afbf
sebsibe23 <sebsibe1990@proton.me>
2023-12-20 00:36:36 -0800

    first

---
+++ 0x1C-makefiles/m.h
@@ -0,0 +1,17 @@
+#ifndef __M_H__
+#define __M_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ * Function Name: print_school
+ * Parameters: None
+ * Return: void
+ * Description: This function prints the name of the school.
+ * The exact behavior depends on the function implementation,
+ * which is not shown here.
+ */
+void print_school(void);
+
+#endif
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+#include "m.h"
+
+/**
+ * main - The Entry point of the program
+ *
+ * Return: Always EXIT_SUCCESS, indicating
+ * the program has executed successfully
+ */
+int main(void)
+{
+	print_school(); /* Call the print_school function */
+	return (EXIT_SUCCESS); /* Return success status */
+}
